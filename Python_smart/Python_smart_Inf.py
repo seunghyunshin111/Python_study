4354554a9ee1f15362099e4148b6980183c0868a
SH <sundovee@gmail.com>
2020-01-27 20:19:19 +0900

    0220

---
+++ Python_smart/Python_smart_Inf.py
@@ -0,0 +1,8 @@
+Python 3.8.0 (tags/v3.8.0:fa919fd, Oct 14 2019, 19:21:23) [MSC v.1916 32 bit (Intel)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> "Hello, World!"
+'Hello, World!'
+>>> Myname='크리스탈'
+>>> "Je ma'alelle" + Myname
+"Je ma'alelle크리스탈"
+>>> 
